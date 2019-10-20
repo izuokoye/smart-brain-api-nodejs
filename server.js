33b1a872958cf9a4908494f91bc00c86ac1afee6
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-10-20 12:35:46 +0100

    add process.env to the server.js

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.post('/fetchClarifaiApi', image.handleApiCall(clarifai));
 
 
 // start the server
-app.listen(3001, () => {
-   console.log('Server running on port 3001.');
+app.listen(process.env.PORT || 3001, () => {
+   console.log(`Server running on port ${process.env.PORT}.`);
 });
